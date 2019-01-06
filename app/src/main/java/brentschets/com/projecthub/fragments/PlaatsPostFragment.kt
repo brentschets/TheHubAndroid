1faf5099dcca1d4a6ede59e598b6e2737f8befac
Brent Schets <brent.schets.y9143@student.hogent.be>
2019-01-06 14:19:52 +0100

    removed unused imports

--- app/src/main/java/brentschets/com/projecthub/fragments/PlaatsPostFragment.kt
+++ app/src/main/java/brentschets/com/projecthub/fragments/PlaatsPostFragment.kt
@@ -6,11 +6,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import brentschets.com.projecthub.R
 import kotlinx.android.synthetic.main.fragment_plaats_post.*
 import brentschets.com.projecthub.viewmodels.*
-import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class PlaatsPostFragment : Fragment() {
